da992d88229d4e195a167d79278518ce2f267ff0
Luiz Rogério Bengozi <luiz.bengozi@tecnospeed.com.br>
2018-05-17 17:42:22 -0300

    Adiciona Demostrações API/OCX

--- demonstracoes/API/Boleto Php API/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = "http://localhost/PhpApi/"; //caminho da URL
-$DirArq = 'Portable-Files/'; //caminho da pasta principal
-
-error_reporting(E_ERROR | E_PARSE); //Remove os notices do php
